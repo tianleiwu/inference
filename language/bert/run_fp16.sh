b9c7c212a58a3c27308cdc89cfc12d55b6e99aed
Tianlei Wu <tlwu@microsoft.com>
2020-09-15 16:47:35 -0700

    update fp16 sh

--- language/bert/run_fp16.sh
+++ language/bert/run_fp16.sh
@@ -1,19 +1,19 @@
-export MLPERF_DTYPE=int8
+export MLPERF_DTYPE=fp16
 
 export MLPERF_BATCH=3
-./run_batch.sh
+sh ./run_batch.sh
 
 export MLPERF_BATCH=4
-./run_batch.sh
+sh ./run_batch.sh
 
 export MLPERF_BATCH=5
-./run_batch.sh
+sh ./run_batch.sh
 
 export MLPERF_BATCH=6
-./run_batch.sh
+sh ./run_batch.sh
 
 export MLPERF_BATCH=7
-./run_batch.sh
+sh ./run_batch.sh
 
 export MLPERF_BATCH=8
-./run_batch.sh
+sh ./run_batch.sh

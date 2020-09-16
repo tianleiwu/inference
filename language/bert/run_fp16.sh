8d2ea375777c635667feb0f5562b62b616ede1e0
Tianlei Wu <tlwu@microsoft.com>
2020-09-16 04:01:45 -0700

    update sh file and add default conf files

--- language/bert/run_fp16.sh
+++ language/bert/run_fp16.sh
@@ -1,19 +1,27 @@
 export MLPERF_DTYPE=fp16
 
-export MLPERF_BATCH=3
+cp user_fp16.conf user.conf
+
+export MLPERF_BATCH=1
 sh ./run_batch.sh
 
 export MLPERF_BATCH=4
 sh ./run_batch.sh
 
-export MLPERF_BATCH=5
-sh ./run_batch.sh
-
-export MLPERF_BATCH=6
-sh ./run_batch.sh
-
-export MLPERF_BATCH=7
-sh ./run_batch.sh
-
 export MLPERF_BATCH=8
+sh ./run_batch.sh
+
+export MLPERF_BATCH=16
+sh ./run_batch.sh
+
+export MLPERF_BATCH=20
+sh ./run_batch.sh
+
+export MLPERF_BATCH=24
+sh ./run_batch.sh
+
+export MLPERF_BATCH=28
+sh ./run_batch.sh
+
+export MLPERF_BATCH=32
 sh ./run_batch.sh
--- language/bert/run_int8.sh
+++ language/bert/run_int8.sh
@@ -1,5 +1,17 @@
 export OMP_NUM_THREADS=16
 export OMP_WAIT_POLICY=ACTIVE
 export MLPERF_DTYPE=int8
+
+cp user_int8.conf user.conf
+
+export MLPERF_BATCH=1
+sh ./run_batch.sh
+
 export MLPERF_BATCH=2
+sh ./run_batch.sh
+
+export MLPERF_BATCH=3
+sh ./run_batch.sh
+
+export MLPERF_BATCH=4
 sh ./run_batch.sh
